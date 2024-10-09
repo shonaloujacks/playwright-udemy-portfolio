4ba9b209d1d71d7364a3ba27bf4824bc8b268df3
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2024-10-09 15:08:53 +0100

    adding output folder for artifacts

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,9 +25,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
 
-  reporter: [["html"], ["allure-playwright"]],
-
-  //{ outputFolder: "test-results" }
+  reporter: [["html"], ["allure-playwright", { outputFolder: "test-results" }]],
 
   globalSetup: require.resolve("./utils/global-setup"),
 
