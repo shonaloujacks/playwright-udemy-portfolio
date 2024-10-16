eb4a9f122d183a8b530b0f57916838c3fbd106ca
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2024-10-16 12:06:33 +0100

    Imported API methods from controller logic file and condensed code

--- tests/contact.api.spec.ts
+++ tests/contact.api.spec.ts
@@ -1,25 +1,17 @@
-import { test, expect, APIRequestContext, APIResponse } from "@playwright/test";
+import { test, expect, APIResponse } from "@playwright/test";
 import ContactPage from "../pages/contact.page";
 import apiController from "../controller/api.controller";
 
 test.describe("Contact page", () => {
   let contactPage: ContactPage;
-  let fakerApi: APIRequestContext;
   let randomPerson: APIResponse;
 
-  test.beforeAll(async ({ playwright }) => {
+  test.beforeAll(async () => {
     await apiController.init();
 
-    const response = await fakerApi.get("users");
-    const responseBody = await response.json();
-    randomPerson = responseBody[0];
-
-    const postResponse = await fakerApi.post("/users/1/todos", {
-      data: { title: "Learn Playwight", completed: "false" },
-    });
-
-    const postResponseBody = await postResponse.json();
-    console.log(postResponseBody);
+    randomPerson = await apiController.getUsers();
+    const newUserToDo = await apiController.createUserTodo();
+    console.log(newUserToDo);
   });
 
   test("Fill contact form and verify success message", async ({ page }) => {
@@ -38,19 +30,6 @@ test.describe("Contact page", () => {
       randomPerson["website"]
     );
 
-    // // Add a soft assertion
-    // await expect
-    //   .soft(contactPage.messageInputBox))
-    //   .toHaveText("Failed test message");
-
-    // Helpful way to stop test execution if there are any failures by this point:
-    // expect(test.info().errors.length).toBeLessThan(1);
-
-    // Verify the success message
-
     await expect(contactPage.submissionTxt).toBeVisible();
-
-    //could also use:
-    //     //await expect(submissionText).toHaveText('text=Thanks for contacting us! We will be in touch with you shortly');
   });
 });
